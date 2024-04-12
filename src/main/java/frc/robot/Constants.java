d935898a86bbf0ecb259976124e2fafe0a9b6b42
Coach Brewster <frcriptide4118@pkyonge.ufl.edu>
2024-04-12 14:35:29 -0400

    EOD 4/12/24 -Amp doesn't go in the right direction, no subsystems for the speaker auto.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -284,8 +284,8 @@ public final class Constants {
     public static final double maxModuleSpeed = 4.5;
 
     public static final HolonomicPathFollowerConfig pathFollowerConfig = new HolonomicPathFollowerConfig(
-      new PIDConstants(5.0, 15.0, 20.0), // Translation constants 
-      new PIDConstants(5.0, 0.0, 20.0), // Rotation constants 
+      new PIDConstants(10.0, 15.0, 20.0), // Translation constants 
+      new PIDConstants(10.0, 20.0, 25.0), // Rotation constants 
       maxModuleSpeed, 
       1.04171, // Drive base radius (distance from center to furthest module) 
       new ReplanningConfig()
